fcb3a3698dcd748c511ed1adab90f21b40aba665
inbreeze <a1051826842@gmail.com>
2023-01-09 22:58:03 +0800

    StatePattern 新增 bad e.g.

---
+++ src/main/java/StatePattern/badeg/GumballMachine.java
@@ -0,0 +1,101 @@
+package StatePattern.badeg;
+
+public class GumballMachine {
+    // 1.定义状态常量
+    // 售罄
+    final static int SOLD_OUT = 0;
+    // 没有投入 25 美分
+    final static int NO_QUARTER = 1;
+    // 已投入 25 美分
+    final static int HAS_QUARTER = 2;
+    // 售出糖果
+    final static int SOLD = 3;
+
+    // 2.定义成员变量
+    // 2.1 定义当前状态变量
+    int state = SOLD_OUT;
+    // 2.2 定义糖果个数变量
+    int count;
+
+
+    // 3.构造器初始化
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) {
+            state = NO_QUARTER;
+        }
+    }
+
+    // 4.动作
+    // 4.1 插入 25 美分动作
+    public void insertQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("You can't insert another quarter");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;                        // 状态更新
+            System.out.println("You inserted a quarter");
+        } else if (state == SOLD_OUT) {
+            System.out.println("You can't insert a quarter, because the machine is sold out");
+        } else if (state == SOLD) {
+            System.out.println("Please wait, we're giving you a gumball");
+        }
+    }
+
+    // 4.2 弹出 25 美分动作
+    public void ejectQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("Quarter returned");
+            state = NO_QUARTER;                         // 状态更新
+        } else if (state == NO_QUARTER) {
+            System.out.println("You haven't inserted a quarter");
+        } else if (state == SOLD_OUT) {
+            System.out.println("You haven't inserted a quarter, because the machine is sold out");
+        } else if (state == SOLD) {
+            System.out.println("Sorry, you already turned the crank");
+        }
+    }
+
+    // 4.3 转动曲柄
+    public void turnCrank() {
+        if (state == HAS_QUARTER) {
+            System.out.println("You turned...");
+            state = SOLD;                               // 状态更新
+            dispense();                                 // 发放糖果
+        } else if (state == NO_QUARTER) {
+            System.out.println("You turned but there's no quarter");
+        } else if (state == SOLD_OUT) {
+            System.out.println("You turned but there's no gumballs");
+        } else if (state == SOLD) {
+            System.out.println("Sorry, you just now turned the crank");
+        }
+    }
+
+    // 4.4 发放糖果至槽中
+    private void dispense() {
+        if (state == HAS_QUARTER) {
+            System.out.println("This time no gumball should dispensed");
+        } else if (state == NO_QUARTER) {
+            System.out.println("This time no gumball should dispensed");
+        } else if (state == SOLD_OUT) {
+            System.out.println("This time no gumball should dispensed");
+        } else if (state == SOLD) {
+            System.out.println("A gumball comes rolling out the slot");
+            count -= 1;
+            if (count == 0) {
+                state = SOLD_OUT;
+            } else {
+                state = NO_QUARTER;
+            }
+        }
+    }
+
+    // 5.其他函数
+
+    @Override
+    public String toString() {
+        return "GumballMachine{" +
+                "state=" + state +
+                ", count=" + count +
+                '}';
+    }
+}
---
+++ src/main/java/StatePattern/badeg/Main.java
@@ -0,0 +1,39 @@
+package StatePattern.badeg;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1.初始化 & 打印糖果机状态
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+
+        // 2.插入 25 美分且转动曲柄 & 打印糖果机状态 [-1]
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+        // 3.插入 25 美分且退出 25 美分 & 打印糖果机状态
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        System.out.println(gumballMachine);
+
+        // 4.插入 25 美分且转动曲柄 * 2 且 退出 25 美分 & 打印糖果机状态 [-2]
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+        System.out.println(gumballMachine);
+
+        // 5.连续插入 2 次 25 美分且转动曲柄 且 插入 25 美分且转动曲柄 * 2 & 打印糖果机状态 [-3]
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+        System.out.println("--- Hint: no gumballs ---");
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+    }
+}
